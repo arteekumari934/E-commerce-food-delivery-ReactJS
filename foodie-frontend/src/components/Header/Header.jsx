5ed0d6c69e7fb2ddb71f6a1eebd24f5f4d6278cd
Artee Pandit <arteepandit934@gmail.com>
2025-01-16 17:29:01 +0530

    React Js front end for online  food delivery website

---
+++ foodie-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite Food here</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt fugit eum eligendi, itaque voluptatem ut natus velit temporibus vel possimus suscipit quidem quae culpa sit necessitatibus autem ad quaerat odit ipsa voluptatibus  </p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
